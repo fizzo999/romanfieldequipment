a58316ef7cd9b076511d1d1330caa18e1cbfd8e5
FIZZO PANNOSCH <fizzo999@gmail.com>
2021-01-06 21:25:36 -0800

    javascriptx3 added

---
+++ js/addjs.js
@@ -0,0 +1,4 @@
+
+
+let userName = prompt('Please Enter Your Name');
+document.write('<h3>' + userName + '<h3>');
---
+++ js/addjs002.js
@@ -0,0 +1,8 @@
+
+
+var footergreeting = greeting + userName;
+
+var el = document.getElementById('bottomgreeting');
+el.textContent = 'Attention this text has been altered from the original html content ! ' + footergreeting;
+
+
